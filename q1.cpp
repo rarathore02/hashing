f149f1cb4221c19ce4b28ee9f806413352e8e8ef
rarathore02 <rashikarathore.rr02@gmail.com>
2025-12-01 22:18:04 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+bool containsDuplicate(int nums[], int n) {
+    unordered_set<int> s;
+    for(int i = 0; i < n; i++){
+        if(s.find(nums[i]) != s.end())
+            return true;
+        s.insert(nums[i]);
+    }
+    return false;
+}
+
+int main(){
+    int nums[] = {1,2,3,1};
+    int n = 4;
+    cout << (containsDuplicate(nums, n) ? "true" : "false");
+}
